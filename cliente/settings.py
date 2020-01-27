046407a7060f5ba2c37a615f64a3d4d018c6606c
yomero2030 <171117@ids.upchiapas.edu.mx>
2020-01-27 15:49:04 -0600

    actualize database

--- cliente/settings.py
+++ cliente/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cliente.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'PASSWORD': 'prick456Fa',
+        'HOST': 'cliente-servidor.cw6fwas135on.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+        'NAME': 'postgres'
     }
 }
 
