23f0cb93c7c31de37842445b3a39792db74a6e01
yomero2030 <171117@ids.upchiapas.edu.mx>
2020-02-19 01:21:02 -0600

    swagger haberu

--- cliente/settings.py
+++ cliente/settings.py
@@ -112,7 +112,6 @@ REST_FRAMEWORK = {
 'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework.authentication.TokenAuthentication',),
 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 'PAGE_SIZE': 100,
-#importar 
 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
