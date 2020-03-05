8b706071dfb7dd36afb9a432fc286afd6ec0afe3
yomero2030 <171117@ids.upchiapas.edu.mx>
2020-03-04 19:47:15 -0600

    agrego el ddns

--- cliente/urls.py
+++ cliente/urls.py
@@ -22,7 +22,7 @@ from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Pastebin API')
 
-
+ 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
